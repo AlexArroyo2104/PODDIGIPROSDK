55c2468e306d9f5aec8c6962f47d37fb6a3410fb
Alejandro López Arroyo <alejandrol@digipro.com.mx>
2019-07-29 17:14:53 -0500

    Bridging header

---
+++ PODDIGIPROSDK/Bridging-Header.h
@@ -0,0 +1,25 @@
+//
+//  Bridging-Header.h
+//  Bridging-Header
+//
+//  Created by Jonathan Viloria M on 5/2/19.
+//  Copyright © 2019 Jonathan Viloria M. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import <XMLDictionary.h>
+#import <UIScrollView+EmptyDataSet.h>
+#import <JGProgressHUD-Defines.h>
+#import <JGProgressHUD.h>
+#import <JGProgressHUDAnimation.h>
+#import <JGProgressHUDErrorIndicatorView.h>
+#import <JGProgressHUDFadeAnimation.h>
+#import <JGProgressHUDFadeZoomAnimation.h>
+#import <JGProgressHUDImageIndicatorView.h>
+#import <JGProgressHUDIndeterminateIndicatorView.h>
+#import <JGProgressHUDIndicatorView.h>
+#import <JGProgressHUDPieIndicatorView.h>
+#import <JGProgressHUDRingIndicatorView.h>
+#import <JGProgressHUDShadow.h>
+#import <JGProgressHUDSuccessIndicatorView.h>
